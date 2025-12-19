094f7bcff18d1500d5176a03cbdfc3260c6ef9a5
Maimoon Hossain <ashikasitosh740@gmail.com>
2025-12-19 22:01:16 +0600

    cloudinary updated

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 app.use(cookieParser());
 app.use(
   cors({
@@ -20,7 +22,7 @@ app.use(
 );
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
