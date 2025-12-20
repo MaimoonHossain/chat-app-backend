bea511948e89b9a51a16fe65c610d0e995c1d0af
Maimoon Hossain <ashikasitosh740@gmail.com>
2025-12-20 21:25:46 +0600

    3:50 online users updated

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
+
+// const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "10mb" }));
@@ -24,7 +26,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
